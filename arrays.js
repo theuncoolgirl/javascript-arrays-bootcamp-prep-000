2bb2ae09c0780cf4acf794ed81fbb01896bb008c
Erin Shields <elshields92@gmail.com>
2020-04-16 14:28:37 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -3,3 +3,8 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 function addElementToBeginningOfArray(array, element) {
   return [element, ...array]
 }
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element)
+  return array
+}
