16143bb3a5eda5a27eea82cbe2c4c2ea3eac0099
Erin Shields <elshields92@gmail.com>
2020-04-16 14:20:26 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,7 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 function addElementToBeginningOfArray(array, element) {
-  return ${array}.unshift(${element})
+  let array = ["1"];
+  array.push("foo");
+  console.log(array);
 }
